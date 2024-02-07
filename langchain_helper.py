0d7a8e43d51a6ae0f4d51c8c5f011c08ec712cd3
Edwin Madrigal <emadrig1@uci.edu>
2024-02-06 23:44:54 -0800

    initial setup

---
+++ langchain_helper.py
@@ -0,0 +1,5 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
